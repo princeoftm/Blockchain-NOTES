4fa67c2b364742f7fdd0b78b87438a540e187cf2
Aniruddh rao <90469031+princeoftm@users.noreply.github.com>
2024-03-04 15:53:02 +0530

    exercises added

--- solidity-exercises/Variable-Types/var.sol
+++ solidity-exercises/Variable-Types/var.sol
@@ -1,7 +1,10 @@
 pragma solidity 0.8.0;
 
 contract variables{
-    uint256 public x;
-    uint16 public x1;
-
+    uint256 public u;
+    uint16 public u1;
+    uint32 public u3;
+    string public m;
+    bytes public b;
+    bytes32 public b32;
 }
