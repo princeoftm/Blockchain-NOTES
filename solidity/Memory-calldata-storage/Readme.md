0564bcd16727127c9987bf04e964a24068c3dd53
Aniruddh rao <90469031+princeoftm@users.noreply.github.com>
2024-03-07 23:05:43 +0530

    added memory calls

---
+++ solidity/Memory-calldata-storage/Readme.md
@@ -0,0 +1,9 @@
+STORAGE
+Storage is where all state-variable is stored.
+
+MEMORY
+Memory is reserved for variables that are defined within the scope of a function.
+
+CALLDATA
+Calldata is an immutable, temporary location where function arguments are stored, and behaves mostly like memory.
+
--- solidity/Variable-Types/var.sol
+++ solidity/Variable-Types/var.sol
@@ -2,9 +2,10 @@ pragma solidity 0.8.0;
 
 contract variables{
     uint256 public u;
-    uint16 public u1;
+    uint public u
+    uint16 public u1;//just use uint256 instead of uint16 and uint32 pls uses less gas in the the end
     uint32 public u3;
     string public m;
     bytes public b;
-    bytes32 public b32;
+    bytes32 public b32;//same goes for this
 }
